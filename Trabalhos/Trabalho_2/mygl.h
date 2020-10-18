ee8f5f4c2130e353ec70a086f1969c39f8b19a15
Andrew <andrewdw18@gmail.com>
2020-10-18 18:18:46 -0300

    add Trabalho_2

---
+++ Trabalhos/Trabalho_2/mygl.h
@@ -0,0 +1,16 @@
+#ifndef MYGL_H
+#define MYGL_H
+
+#include <string.h>
+
+#include "core.h"
+#include "frame_buffer.h"
+
+// Declaração da função que chamará as funções implementadas pelo aluno
+void MyGlDraw(void);
+
+//
+// >>> Declare aqui as funções que você implementar <<<
+//
+
+#endif  // MYGL_H
