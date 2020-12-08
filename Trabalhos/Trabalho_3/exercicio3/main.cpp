9500581fc59b84febfb59c4fe2c050624a04d9ca
Andrew Figueiredo <andrewdw18@gmail.com>
2020-12-08 10:57:27 -0300

    Alteração no exercicio 3

--- Trabalhos/Trabalho_3/exercicio3/main.cpp
+++ Trabalhos/Trabalho_3/exercicio3/main.cpp
@@ -66,8 +66,8 @@ void Display(void) {
 
     // Matriz Model ///////////////////////////////////////////////////////////
     // You will have to change the contents of this matrix for the exercises
-    float model_array[16] = {0.33f, 0.0f, 0.0f, 0.0f, 
-                             0.0f, 1.5f, 0.0f, 0.0f, 
+    float model_array[16] = {1.0f, 0.0f, 0.0f, 0.0f, 
+                             0.0f, 1.0f, 0.0f, 0.0f, 
                              0.0f, 0.0f, 1.0f, 0.0f, 
                              0.0f, 0.0f, 0.0f, 1.0f};
     glm::mat4 model_mat = glm::make_mat4(model_array);
@@ -85,8 +85,8 @@ void Display(void) {
     // You will have to change the contents of this matrix for the exercises
     float proj_array[16] = {1.0f, 0.0f, 0.0f, 0.0f, 
                             0.0f, 1.0f, 0.0f, 0.0f, 
-                            0.0f, 0.0f, 1.0f, 0.0f, 
-                            0.0f, 0.0f, 0.0f, 1.0f};
+                            0.0f, 0.0f, 1.0f, -8.0f, 
+                            0.0f, 0.0f, 0.125f, 1.0f};
 
     glm::mat4 proj_mat = glm::make_mat4(proj_array);
 
